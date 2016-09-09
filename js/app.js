e3e2dd3483b603aba1053c5cd05290efa2c43a5a
Liam Kelly <likelly@berkeley.edu>
2016-09-09 13:26:05 -0700

    Incorrect UI router boilerplate

--- js/app.js
+++ js/app.js
@@ -1,42 +1,44 @@
 /**
  * Created by Liam on 9/8/2016.
  */
+(function(angular) {
+    'use strict';
+    var app = angular.module('lck-app', ['ngRoute', 'ui.router']);
 
-var app = angular.module('lck-app', ['ngRoute', 'ui.router']);
+    app.config(['$urlRouterProvider', '$stateProvider', '$httpProvider',
+        function($urlRouterProvider, $stateProvider, $httpProvider) {
+            $urlRouterProvider.otherwise('/home');
 
-app.config(['$urlRouterProvider', '$stateProvider', '$httpProvider',
-    function($urlRouterProvider, $stateProvider, $httpProvider) {
-        $urlRouterProvider.otherwise('/home');
+            $stateProvider.state('base', {
+                abstract: true,
+                template: '<ui-view/>'
+            });
 
-        $stateProvider.state('base', {
-            abstract: true,
-            template: '<ui-view/>'
-        });
+            $stateProvider.state('base.home', {
+                templateUrl: 'html/home.html',
+                controller: 'HomeController',
+                url: '/home'
+            });
 
-        $stateProvider.state('base.home', {
-            templateUrl: 'html/home.html',
-            controller: 'HomeController',
-            url: '/home'
-        });
+            $stateProvider.state('base.games', {
+                templateUrl: 'html/games.html',
+                controller: 'GamesController',
+                url: '/games'
+            });
 
-        $stateProvider.state('base.games', {
-            templateUrl: 'html/games.html',
-            controller: 'GamesController',
-            url: '/games'
-        });
+            //copy template later for further views
+        }
+    ]);
 
-        //copy template later for further views
-    }
-]);
+    app.controller('HomeController', ['$scope',
+        function($scope) {
+            //homepage js goes here
+        }
+    ]);
 
-app.controller('HomeController', ['$scope',
-    function($scope) {
-        //homepage js goes here
-    }
-]);
-
-app.controller('GamesController', ['$scope',
-    function($scope) {
-        //homepage js goes here
-    }
-]);
+    app.controller('GamesController', ['$scope',
+        function($scope) {
+            //homepage js goes here
+        }
+    ]);
+});
